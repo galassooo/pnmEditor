446bf6a5da6b89c243c60327b03c000cf255d80b
Galasso Martina <martina.galasso@student.supsi.ch>
2024-11-25 17:49:55 +0100

    simplified fxml structure

--- frontend/src/test/java/org/supsi/view/AbstractGUITest.java
+++ frontend/src/test/java/org/supsi/view/AbstractGUITest.java
@@ -3,6 +3,7 @@ package org.supsi.view;
 
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.supsi.MainFx;
 import org.testfx.framework.junit5.ApplicationTest;
 
@@ -15,6 +16,7 @@ abstract public class AbstractGUITest extends ApplicationTest {
     protected static final Logger LOGGER = Logger.getAnonymousLogger();
 
     protected int stepNo;
+
 
     @BeforeAll
     public static void setupSpec() {
@@ -35,10 +37,9 @@ abstract public class AbstractGUITest extends ApplicationTest {
     }
 
     public void start(final Stage stage) throws Exception {
-        final MainFx main = new MainFx();
-        stage.toFront();
-        stage.setAlwaysOnTop(true);
-        main.start(stage);
+            final MainFx main = new MainFx();
+            stage.toFront();
+            stage.setAlwaysOnTop(true);
+            main.start(stage);
     }
-
 }
--- frontend/src/test/java/org/supsi/view/basic/MenuBarTest.java
+++ frontend/src/test/java/org/supsi/view/basic/MenuBarTest.java
@@ -1,14 +1,23 @@
 package org.supsi.view.basic;
 
 import com.sun.javafx.scene.control.ContextMenuContent;
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.supsi.model.state.IStateModel;
+import org.supsi.model.state.StateModel;
 import org.supsi.view.AbstractGUITest;
+import org.testfx.util.WaitForAsyncUtils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.testfx.api.FxAssert.verifyThat;
 
+import static org.testfx.matcher.base.NodeMatchers.isDisabled;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
 
 public class MenuBarTest extends AbstractGUITest {
@@ -17,9 +26,13 @@ public class MenuBarTest extends AbstractGUITest {
     public void walkThrough() {
         testMainScene();
         testClickOnMenuItem();
+        testFileMenuItems();
+        testEditMenuPreferences();
+        testHelpMenuAbout();
+        testExportMenuInitialState();
     }
 
-    void testMainScene() {
+    private void testMainScene() {
         step("menu bar structure", () -> {
             verifyThat("#fileMenu", isVisible());
             verifyThat("#edit", isVisible());
@@ -27,10 +40,9 @@ public class MenuBarTest extends AbstractGUITest {
         });
     }
 
-    void testClickOnMenuItem() {
+    private void testClickOnMenuItem() {
         step("file menu", () -> {
-            sleep(SLEEP_INTERVAL);
-            clickOn("#fileMenu");
+            clickOn("#root");
             sleep(SLEEP_INTERVAL);
             clickOn("#fileMenu");
             sleep(SLEEP_INTERVAL);
@@ -38,6 +50,67 @@ public class MenuBarTest extends AbstractGUITest {
 
             sleep(SLEEP_INTERVAL);
             verifyThat("#openMenuItem", isVisible());
+
+            press(KeyCode.ESCAPE);
         });
     }
+
+    private void testFileMenuItems() {
+        step("file menu disabled bindings", () -> {
+            sleep(SLEEP_INTERVAL);
+            clickOn("#fileMenu");
+            sleep(SLEEP_INTERVAL);
+
+            MenuItem saveMenuItem = lookup("#saveMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+            MenuItem saveAsMenuItem = lookup("#saveAsMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+
+            assertTrue(saveMenuItem.isDisable());
+            assertTrue(saveAsMenuItem.isDisable());
+
+            press(KeyCode.ESCAPE);
+        });
+    }
+
+
+    private void testEditMenuPreferences() {
+        step("preferences dialog", () -> {
+            clickOn("#edit");
+            clickOn("#preferences");
+
+            // Verify preferences dialog components
+            verifyThat("#choiceBox", isVisible());
+            verifyThat("#errorCB", isVisible());
+            verifyThat("#warningCB", isVisible());
+            verifyThat("#debugCB", isVisible());
+
+            clickOn("#closeButton");
+            press(KeyCode.ESCAPE);
+        });
+    }
+
+    private void testHelpMenuAbout() {
+        step("about dialog", () -> {
+            clickOn("#help");
+            clickOn("About");
+
+            verifyThat("#date", isVisible());
+            verifyThat("#version", isVisible());
+            verifyThat("#developer", isVisible());
+
+            clickOn("#closeButton");
+            press(KeyCode.ESCAPE);
+        });
+    }
+
+    private void testExportMenuInitialState() {
+        step("export menu initial disable state", () -> {
+            clickOn("#fileMenu");
+
+            // Should be disabled initially with no image loaded
+            verifyThat("#exportMenu", isDisabled());
+
+            press(KeyCode.ESCAPE);
+        });
+    }
+
 }

e08e00ecc1ad781dd8decab94c36568fcc0a7361
Galasso Martina <martina.galasso@student.supsi.ch>
2024-11-28 08:42:30 +0100

    changed headless config

--- frontend/src/test/java/org/supsi/view/AbstractGUITest.java
+++ frontend/src/test/java/org/supsi/view/AbstractGUITest.java
@@ -38,9 +38,7 @@ abstract public class AbstractGUITest extends ApplicationTest {
     public static void setupSpec() {
         if (Boolean.getBoolean("headless")) {
             System.setProperty("testfx.robot", "glass");
-            System.setProperty("testfx.headless", "true");
             System.setProperty("prism.order", "sw");
-            System.setProperty("prism.text", "t2k");
             System.setProperty("java.awt.headless", "true");
         }
 
